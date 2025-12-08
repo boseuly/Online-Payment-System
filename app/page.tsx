cfffa89f588832045212723e444592849431cd32
elena <tool0923@naver.com>
2025-12-08 14:47:17 +0900

    쇼핑몰 메인 화면 디자인

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+
+"use client"
 import Image from "next/image";
 import MainHeader from '../components/common/MainHeader';
 import '../styles/css/main.css'
+import '../styles/css/style.css'
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import { SwiperSlide, Swiper } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper/modules"; 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 
 export default function Home() {
@@ -12,6 +21,36 @@ export default function Home() {
 
     
     {/* body */}
+    <div className="main-body">
+      <Swiper 
+      className="main-swiper wd-100 hg-100"
+      slidesPerView={1}
+      effect={'fade'}
+      modules={[Autoplay, EffectFade]}
+      autoplay={{
+        delay: 4000,       // 3초마다 자동 이동
+        disableOnInteraction: false, // 유저가 터치해도 계속 자동
+      }}
+      speed={2000}   // ← 페이드 속도(1.5초)
+      fadeEffect={{ crossFade: true }}
+      loop={true}>
+        <SwiperSlide>
+          <div className="wd-100 hg-100">
+            <a href="#">
+              <span className="main-background-image-1"></span>
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="wd-100 hg-100">
+            <a href="#">
+              <span className="main-background-image-2"></span>
+            </a>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+
 
     
     {/* footer */}
--- components/common/MainHeader.tsx
+++ components/common/MainHeader.tsx
@@ -5,9 +5,9 @@ export default function MainHeader(){
     return (
         <>
             {/* 로그인을 한 상태 */}
-            <header>
+            <header className="p-fixed wd-100 z-index-100 ">
                 <div className="d-flex m-10 ">
-                    <div>dsdfsdf</div>
+                    <div className="pl-50 wd-165px"><img src={"/img/01_logo_white.png"}/></div>
                     <div></div>
                     <div></div>
                 </div>
